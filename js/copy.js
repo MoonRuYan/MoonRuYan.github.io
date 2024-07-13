0005811e038b27967c855193cd6bc4a6ad30d744
MoonRuYan <13976227625@163.com>
2024-07-13 09:17:33 +0800

    Site updated: 2024-07-13 09:17:32

--- js/copy.js
+++ js/copy.js
@@ -29,7 +29,7 @@ document.addEventListener("copy", function () {
   
   // f12提醒但不禁用
   document.onkeydown = function (e) {
-    if (123 == e.keyCode || (e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode)) || (e.ctrlKey && 85 === e.keyCode)) {
+    if (123 === e.keyCode || (e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode)) || (e.ctrlKey && 85 === e.keyCode)) {
       debounce(function () {
         new Vue({
           data: function () {
